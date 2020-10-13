906430876e612a57a786767b72ae02d20628f225
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-10-13 14:25:35 -0600

    Initial commit

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import logo from "../logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <body>
+      <header>find my food</header>
+    </body>
+  );
+}
+
+export default App;
---
+++ src/RestaurantContainer/RestaurantContainer.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export const RestaurantContainer = () => {
+  return <main className="restaurant-container"></main>;
+};
+
+export default RestaurantContainer;
