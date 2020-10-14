3452bf91d3259f583e1ebd8798155195c29be926
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-10-14 11:43:44 -0600

    Create fetch and populate info on page

--- src/RestaurantContainer/RestaurantContainer.js
+++ src/RestaurantContainer/RestaurantContainer.js
@@ -1,7 +1,37 @@
 import React, { Component } from "react";
+import RestaurantRow from "../RestaurantRow/RestaurantRow";
 
-export const RestaurantContainer = () => {
-  return <main className="restaurant-container"></main>;
-};
+export class RestaurantContainer extends Component {
+  state = {
+    restaurants: [],
+  };
+
+  componentDidMount() {
+    fetch("https://code-challenge.spectrumtoolbox.com/api/restaurants", {
+      headers: {
+        Authorization: "Api-Key q3MNxtfep8Gt",
+      },
+    })
+      .then((response) => response.json())
+      .then((restaurants) => this.getRestaurantList(restaurants))
+      .catch((error) => console.log(error));
+  }
+
+  getRestaurantList(restaurants) {
+    const restaurantList = restaurants.map((res) => {
+      return <RestaurantRow info={res} />;
+    });
+    this.setState({ restaurants: [...restaurantList] });
+  }
+
+  render() {
+    return (
+      <main className="restaurant-container">
+        <h3>Restaurants:</h3>
+        {this.state.restaurants}
+      </main>
+    );
+  }
+}
 
 export default RestaurantContainer;
