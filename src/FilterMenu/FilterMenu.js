bf7ff44816c01b1491e438780d01c04f484375e9
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-10-15 11:47:03 -0600

    Add dual filtering functionality

--- src/FilterMenu/FilterMenu.js
+++ src/FilterMenu/FilterMenu.js
@@ -5,30 +5,63 @@ import { connect } from "react-redux";
 export class FilterMenu extends Component {
   state = {
     restaurants: [],
+    genre: "",
+    state: "",
   };
 
-  filterRestaurants(type, search) {
-    const results = this.props.restaurants.filter((res) => {
-      return res[type] === search;
-    });
+  filterRestaurants = (e) => {
+    e.preventDefault();
+    const { genre, state } = this.state;
+    const { restaurants, searchRestaurants } = this.props;
+    let results = [];
+    if (genre) {
+      results = restaurants[0].filter((res) => {
+        return res.genre.toLowerCase().includes(genre.toLowerCase());
+      });
+    }
+    if (state) {
+      results = restaurants[0].filter((res) => {
+        return res.state.toLowerCase().includes(state.toLowerCase());
+      });
+    }
+    if (genre && state) {
+      results = restaurants[0]
+        .filter((res) => {
+          return res.genre.toLowerCase().includes(genre.toLowerCase());
+        })
+        .filter((res) => {
+          return res.state.toLowerCase().includes(state.toLowerCase());
+        });
+    }
     searchRestaurants(results);
-  }
+  };
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
   render() {
     return (
       <aside className="filter-aside">
         <form className="filter-form">
-          <div className="name-div">
-            <label>Name</label>
-            <input className="name-input"></input>
-          </div>
           <div className="state-div">
             <label>State</label>
-            <input className="state-input"></input>
+            <input
+              className="state-input"
+              name="state"
+              onChange={this.handleChange}
+            ></input>
           </div>
           <div className="genre-div">
             <label>Genre</label>
-            <input className="genre-input"></input>
+            <input
+              className="genre-input"
+              name="genre"
+              onChange={this.handleChange}
+            ></input>
+            <button type="submit" onClick={this.filterRestaurants}>
+              Submit
+            </button>
           </div>
         </form>
       </aside>
