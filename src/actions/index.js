def7c27747775b8a968563ca6b314bbb777e5331
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-10-15 19:17:26 -0600

    Pass local state info to redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,18 @@ export const searchRestaurants = (search) => ({
   type: "SEARCH_RESTAURANTS",
   search,
 });
+
+export const resetRestaurants = (reset) => ({
+  type: "RESET_RESTAURANTS",
+  reset,
+});
+
+export const changeCurrentPage = (page) => ({
+  type: "CHANGE_PAGE",
+  page,
+});
+
+export const changeSelection = (places) => ({
+  type: "NEW_PAGE",
+  places,
+});
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from "redux";
 import restaurantReducer from "./restaurantReducer";
+import pageReducer from "./pageReducer";
+import currentPageReducer from "./currentPageReducer";
 
 const rootReducer = combineReducers({
   restaurants: restaurantReducer,
+  currentPages: pageReducer,
+  currentPage: currentPageReducer,
 });
 
 export default rootReducer;
