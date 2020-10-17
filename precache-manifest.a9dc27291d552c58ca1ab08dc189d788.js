8a17880f6c7c2d916b09fb49fd55d1e9abfa50ed
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-10-16 19:13:52 -0600

    Updates

---
+++ precache-manifest.a9dc27291d552c58ca1ab08dc189d788.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e657b012afd28965fe0b3c761823c6c",
+    "url": "/find-my-food/index.html"
+  },
+  {
+    "revision": "342d233326526951e4de",
+    "url": "/find-my-food/static/css/main.e18be82e.chunk.css"
+  },
+  {
+    "revision": "7e389553ce91a371a2e9",
+    "url": "/find-my-food/static/js/2.e01211a0.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/find-my-food/static/js/2.e01211a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "342d233326526951e4de",
+    "url": "/find-my-food/static/js/main.8001d4c9.chunk.js"
+  },
+  {
+    "revision": "a7eb304ce847226f4c4c",
+    "url": "/find-my-food/static/js/runtime-main.a49def67.js"
+  }
+]);
