b51586c3d2e266cb091bf10d6f037c826748d371
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-10-19 11:22:09 -0600

    Add snapshots for App and Row

---
+++ src/RestaurantRow/RestaurantRow.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import RestaurantRow from "./RestaurantRow";
+import { shallow } from "enzyme";
+
+describe("RestaurantRow", () => {
+  let props = {
+    genre: "Asian",
+    name: "Uchi",
+    city: "Denver",
+    state: "CO",
+    telephone: "(000)123-4567",
+    address1: "Somewhere in rino, I forget",
+    id: "98whef982eifhw",
+  };
+  let wrapper = shallow(<RestaurantRow info={props} />);
+  it("should render the component", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
